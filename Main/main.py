1716e08c5d22881a10383c4e0eddecc7a3b21137
rmn8054 <bhayling55@gmail.com>
2025-01-20 10:59:14 +0000

    updated files

--- Main/main.py
+++ Main/main.py
@@ -3,3 +3,7 @@ from room import Room
 kitchen = Room("Kitchen")
 ballroom = Room("Ballroom")
 dining_hall = Room("Dining Hall")
+
+kitchen.set_description("A dark and dirty room buzzing with flies")
+
+kitchen.describe()
--- Main/room.py
+++ Main/room.py
@@ -2,8 +2,9 @@ class Room:
 
     # constructor
     def __init__ (self, room_name):
-        self.name = room_name
+        self.name = None
         self.description = None
+        self.linked_room = {}
 
     # creating getter and setter
     def get_description (self):
@@ -11,3 +12,12 @@ class Room:
     
     def set_description (self, room_description):
         self.description = room_description
+
+    def describe(self):
+        print(self.description)
+
+    def get_name(self):
+        return self.name
+    
+    def set_name(self, room_name):
+        self.name = room_name
