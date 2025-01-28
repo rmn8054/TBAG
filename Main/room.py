5095f8f7fa99d7edc4acbcae668ffb2f5a593d59
rmn8054 <bhayling55@gmail.com>
2025-01-28 15:03:23 +0000

    Made a start on directions

--- Main/room.py
+++ Main/room.py
@@ -4,20 +4,21 @@ class Room:
     def __init__ (self, room_name):
         self.name = None
         self.description = None
-        self.linked_room = {}
+        self.linked_rooms = {}
 
     # creating getter and setter
     def get_description (self):
         return self.description
-    
     def set_description (self, room_description):
         self.description = room_description
 
     def describe(self):
         print(self.description)
-
-    def get_name(self):
-        return self.name
     
     def set_name(self, room_name):
         self.name = room_name
+    def get_name(self):
+        return self.name
+        
+    def link_room(self, room_to_link, direction):
+        self.linked_rooms[direction] = room_to_link
